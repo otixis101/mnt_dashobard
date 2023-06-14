77514b0be74b6bb396eaa8bebc320fe513494d0a
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-06-14 05:05:37 +0100

    feat: set up design system config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,45 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "hsla(255, 83%, 53%, 1)",
+        midpup: "hsla(254, 85%, 92%, 1)",
+
+        mnt: {
+          green: "hsla(140, 60%, 50%, 1)",
+          orange: "hsla(20, 78%, 51%, 1)",
+          white: "hsla(0, 0%, 95%, 1)",
+        },
+        pale: {
+          purple: "hsla(249, 27%, 61%, 1)",
+          orange: "hsla(30, 100%, 71%, 1)",
+          yellow: "hsla(53, 75%, 94%, 1)",
+        },
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "15px",
+          sm: "0",
+          lg: "0",
+          xl: "0",
+          "2xl": "0",
+        },
+        screens: {
+          sm: "540px",
+          md: "720px",
+          lg: "960px",
+          xl: "1140px",
+          "2xl": "1260px",
+        },
+      },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1200px",
+        "2xl": "1440px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -14,5 +53,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-radix")()],
 };
