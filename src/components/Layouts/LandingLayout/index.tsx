b330125671207d75b9c9c35c4aa08cec4f31c041
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-08-02 20:40:52 +0100

    misc: minor copy updates

--- src/components/Layouts/LandingLayout/index.tsx
+++ src/components/Layouts/LandingLayout/index.tsx
@@ -1,8 +1,8 @@
 import Footer from "@/components/molecules/Footer";
 import NavBar from "@/components/molecules/NavBar";
-import React, { useEffect, useState } from "react";
-import Payment from "../../molecules/Payment";
-import { Dialog, DialogContent } from "../../atoms/Popup/TestPopUp";
+import React, { useEffect } from "react";
+// import Payment from "../../molecules/Payment";
+// import { Dialog, DialogContent } from "../../atoms/Popup/TestPopUp";
 import SEO from "./SEO";
 
 interface RootLayoutProps {
@@ -11,35 +11,35 @@ interface RootLayoutProps {
   type: TemplateType;
 }
 
-const Trigger = () => {
-  const [open, setOpen] = useState(true);
-  return (
-    <Dialog open={open} onOpenChange={setOpen} modal>
-      <DialogContent>
-        <Payment
-          btnText="Continue with free account"
-          subscribeText="Subscribe to premium"
-        />
-      </DialogContent>
-    </Dialog>
-  );
-};
+// const Trigger = () => {
+//   const [open, setOpen] = useState(true);
+//   return (
+//     <Dialog open={open} onOpenChange={setOpen} modal>
+//       <DialogContent>
+//         <Payment
+//           btnText="Continue with free account"
+//           subscribeText="Subscribe to premium"
+//         />
+//       </DialogContent>
+//     </Dialog>
+//   );
+// };
 
 const LandingLayout = ({ children, seo, type }: RootLayoutProps) => {
-  const [popUp, setPopUp] = useState(false);
-  const subscriptionPopUp = () => {
-    setTimeout(() => {
-      setPopUp(true);
-    }, 3000);
-  };
+  // const [popUp, setPopUp] = useState(false);
+  // const subscriptionPopUp = () => {
+  //   setTimeout(() => {
+  //     setPopUp(true);
+  //   }, 3000);
+  // };
 
   useEffect(() => {
-    subscriptionPopUp();
+    // subscriptionPopUp();
   }, []);
 
   return (
     <div className="bg-white text-black">
-      {popUp && <Trigger />}
+      {/* {popUp && <Trigger />} */}
       <SEO {...seo} ogType={type} />
 
       <NavBar />
