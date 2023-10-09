65c78821d0f975fdc43a9b2ad3c7ee16bdf4cf1d
Nweke kenechukwu christopher <72747787+icodejsx@users.noreply.github.com>
2023-10-09 11:50:24 +0100

    adding  Document Vaults UI

---
+++ src/components/organisms/DocumentVault/UploadVault/index.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable indent */
+import Button from "@/components/atoms/Button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const UploadVault = () => (
+    <div className=" hidden lg:flex flex-col gap-5 justify-between py-10 items-center bg-[#F3F3F3] col-span-2 rounded-3xl">
+
+        <div className=" flex flex-col gap-8 w-full" >
+            <div className="flex justify-center">
+
+                <Button className="w-auto px-3 text-sm  flex justify-center ">
+                    + Upload File
+                </Button>
+            </div>
+            <div className="flex flex-col  justify-end text-left w-full pl-4 gap-7  ">
+
+                <Link href="a" className="flex gap-4 ">
+                    <Image src="/assets/vaulticon.svg" alt="" width={20} height={20} />
+                    <p>Document Vault</p>
+                </Link>
+
+                <Link href="a" className="flex gap-4">
+                    <Image src="/assets/user.svg" alt="" width={20} height={20} />
+                    <p>Share With Me </p>
+                </Link>
+            </div>
+        </div>
+        <div className="text-left w-full pl-4 font-bold gap-1 flex flex-col mb-16  ">
+            <p>Storage Space</p>
+            <Image src="/assets/line.svg" alt="" width={150} height={20} />
+            <p className="font-normal text-xs">0gb of 25gb used</p>
+
+        </div>
+
+
+    </div>
+);
+
+export default UploadVault;
