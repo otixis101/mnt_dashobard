6b95831b92902705e1dcfc9851781169723a30a2
Nweke kenechukwu christopher <72747787+icodejsx@users.noreply.github.com>
2023-10-11 14:43:08 +0100

    done  started working on the upload file

--- src/components/organisms/DocumentVault/VaultsRecentUpload/index.tsx
+++ src/components/organisms/DocumentVault/VaultsRecentUpload/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 
 const VaultsRecentUpload = () => (
+
   <div className="col-span-6 lg:bg-[#F3F3F3] rounded-3xl lg:py-10 lg:px-7  flex flex-col gap-5 font-">
 
     <form action="search_results.html" method="GET" className=" hidden lg:flex gap-3 justify-end ">
@@ -57,60 +58,7 @@ const VaultsRecentUpload = () => (
 
   </div>
 
-    <div className="col-span-6 lg:bg-[#F3F3F3] rounded-3xl lg:py-10 lg:px-7  flex flex-col gap-5 font-">
 
-        <form action="search_results.html" method="GET" className=" hidden lg:flex gap-3 justify-end ">
-            <input className="rounded-full w-80 bg-gray-400" type="text" name="search" placeholder="Search..." />
-            <button type="button"
-                className="rounded-lg bg-black p-1 px-8 text-white">Search</button>
-        </form>
-        <p className="font-bold">Recent Uploads</p>
-
-        <div className=" flex flex-wrap lg:gap-10 gap-4  ">
-            <div className="flex flex-col gap-1 items-center justify-center " >
-                <Image src="/assets/docs.svg" alt="docu" width={50} height={50} />
-                <p className="lg:text-xs text-[11px]">Birth Certificate.PDF</p>
-            </div>
-            <div className="flex flex-col gap-1 items-center " >
-                <Image src="/assets/docs.svg" alt="docu" width={50} height={50} />
-                <p className=" md:text-xs text-[11px]">Birth Certificate.PDF</p>
-            </div>
-            <div className="flex flex-col gap-1 items-center " >
-                <Image src="/assets/docs.svg" alt="docu" width={50} height={50} />
-                <p className=" md:text-xs text-[11px]">Birth Certificate.PDF</p>
-            </div>
-            <div className="flex flex-col gap-1 items-center " >
-                <Image src="/assets/docs.svg" alt="docu" width={50} height={50} />
-                <p className=" md:text-xs text-[11px]">Birth Certificate.PDF</p>
-            </div>
-            <div className="flex flex-col gap-1 items-center " >
-                <Image src="/assets/docs.svg" alt="docu" width={50} height={50} />
-                <p className=" md:text-xs text-[11px]">Birth Certificate.PDF</p>
-            </div>
-            <div className="flex flex-col gap-1 items-center " >
-                <Image src="/assets/docs.svg" alt="docu" width={50} height={50} />
-                <p className=" md:text-xs text-[11px]">Birth Certificate.PDF</p>
-            </div>
-            <div className="flex flex-col gap-1 items-center " >
-                <Image src="/assets/docs.svg" alt="docu" width={50} height={50} />
-                <p className=" md:text-xs text-[11px]">Birth Certificate.PDF</p>
-            </div>
-            <div className="flex flex-col gap-1 items-center " >
-                <Image src="/assets/docs.svg" alt="docu" width={50} height={50} />
-                <p className=" md:text-xs text-[11px]">Birth Certificate.PDF</p>
-            </div>
-            <div className="flex flex-col gap-1 items-center " >
-                <Image src="/assets/docs.svg" alt="docu" width={50} height={50} />
-                <p className=" md:text-xs text-[11px]">Birth Certificate.PDF</p>
-            </div>
-            <div className="flex flex-col gap-1 items-center " >
-                <Image src="/assets/docs.svg" alt="docu" width={50} height={50} />
-                <p className=" md:text-xs text-[11px]">Birth Certificate.PDF</p>
-            </div>
-
-        </div>
-
-    </div>
 
 );
 
