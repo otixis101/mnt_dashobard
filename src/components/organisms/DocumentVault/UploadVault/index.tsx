86b5236bc9e6057e35357adc7b41c7b94e4f5b17
cipher-tech <nickchibuikem@gmail.com>
2023-12-01 02:38:40 +0100

    formatted code

--- src/components/organisms/DocumentVault/UploadVault/index.tsx
+++ src/components/organisms/DocumentVault/UploadVault/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Image from "next/image";
-import Link from "next/link";
+// import Image from "next/image";
+// import Link from "next/link";
 
 import UploadLink from "./UploadLink";
 
@@ -14,19 +14,19 @@ const UploadVault = () => (
       <div className="flex justify-center">
         <UploadLink />
       </div>
-      <div className="flex flex-col justify-end text-left w-full pl-4 gap-7">
+      {/* <div className="flex flex-col justify-end text-left w-full pl-4 gap-7">
         <Link href="a" className="flex gap-4 ">
           <Image src="/assets/vaulticon.svg" alt="" width={20} height={20} />
           <p>Document Vault</p>
         </Link>
 
-      </div>
+      </div> */}
     </div>
-    <div className="text-left w-full pl-4 font-bold gap-1 flex flex-col mb-16">
+    {/* <div className="text-left w-full pl-4 font-bold gap-1 flex flex-col mb-16">
       <p>Storage Space</p>
       <Image src="/assets/line.svg" alt="" width={150} height={20} />
       <p className="font-normal text-xs">0gb of 25gb used</p>
-    </div>
+    </div> */}
   </div>
 );
 
